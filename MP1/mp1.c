7000cd9d7fe4907f447621a5bccff05dd5193272
Fgoyal <fgoyal2@illinois.edu>
2021-02-18 04:46:15 -0600

    Followed RTioW tutorial to gradient background and colorful circle

--- MP1/mp1.c
+++ MP1/mp1.c
@@ -1,24 +1,60 @@
+#include "color.h"
+#include "ray.h"
+#include "vec3.h"
 #include <iostream>
 
+double hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = r.origin() - center;
+    auto a = r.direction().length_squared();
+    auto half_b = dot(oc, r.direction());
+    auto c = oc.length_squared() - radius * radius;
+    auto discriminant = half_b * half_b - a * c;
+    if (discriminant < 0) {
+        return -1.0;
+    } else {
+        return (-half_b - sqrt(discriminant)) / (a);
+    }
+}
+
+color ray_color(const ray& r) {
+    double hit = hit_sphere(point3(0, 0, -1), 0.5, r);
+    if (hit > 0.0) {
+        vec3 N = unit_vector(r.at(hit) - vec3(0,0,-1));
+        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+    }
+    vec3 unit_direction = unit_vector(r.direction());
+    hit = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - hit) * color(1.0, 1.0, 1.0) + hit * color(0.5, 0.7, 1.0);
+}
 
 int main() {
-    const int image_width = 256;
-    const int image_height = 256;
 
+    // Image
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = 500;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    // Camera
+    float viewport_height = 2.0;
+    float viewport_width = aspect_ratio * viewport_height;
+    float focal_length = 1.0;
+
+    point3 origin = point3(0,0,0);
+    vec3 horizontal = vec3(viewport_width, 0, 0);
+    vec3 vertical = vec3(0, viewport_height, 0);
+    vec3 lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
+
+    // Render
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = image_height - 1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            auto r = double(i) / (image_width - 1);
-            auto g = double(j) / (image_height - 1);
-            auto b = 0.25;
-
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            auto u = double(i) / (image_width - 1);
+            auto v = double(j) / (image_height - 1);
+            ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+            color pixel_color = ray_color(r);
+            write_color(std::cout, pixel_color);
         }
     }
 
