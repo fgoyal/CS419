1527ed91b932135ded78e62f5496c6d1af96997a
Fgoyal <fgoyal2@illinois.edu>
2021-02-22 13:29:18 -0600

    Wrote function to generate multi jitter mask

--- MP1/utils.h
+++ MP1/utils.h
@@ -3,8 +3,10 @@
 
 #include "ray.h"
 #include "vec3.h"
-#include <time.h>
+#include <ctime>
 #include <cstdlib>
+#include <random>
+#include <map>
 
 // From RTioW
 inline double random_double() {
@@ -19,10 +21,47 @@ inline double random_double(double min, double max) {
     return min + (max-min)*random_double();
 }
 
-inline double random_int(int min, int max) {
-    // Returns a random real in [min,max).
-    srand(time(0));
+inline int random_int(int min, int max) {
+    // Returns a random int in [min,max).
+    // srand(time(NULL));
     return rand() % max + min;
+}
+
+inline bool** get_multi_jitter_mask(int fine_size) {
+    int coarse_size = (int) sqrt(fine_size);
+    
+    std::map<int, bool> row;
+    std::map<int, bool> col;
+
+    bool** sample;
+    sample = new bool*[fine_size];
+    for (int i = 0; i < fine_size; i++) {
+        sample[i] = new bool[fine_size];
+        for (int j = 0; j < fine_size; j++) {
+            sample[i][j] = false;
+        }
+    }
+    srand(time(NULL));
+    for (int i = 0; i < coarse_size; i++) {
+        for (int j = 0; j < coarse_size; j++) {
+            std::cerr << i << " " << j << "\n";
+            int x;
+            int y;
+            do {
+                x = random_int(0, coarse_size) + (i * coarse_size);
+            } while (row.count(x) > 0);
+            row.insert(std::pair<int,bool>(x,true));
+            
+            do {
+                y = random_int(0, coarse_size) + (j * coarse_size);
+            } while (col.count(y) > 0);
+            col.insert(std::pair<int,bool>(y,true));
+            
+            std::cerr << x << " " << y << "\n\n";
+            sample[x][y] = true;
+        }
+    }
+    return sample;
 }
 
 #endif
