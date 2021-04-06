392f7bbb97c21fbc2395c67438ea620a525a819a
Fgoyal <fgoyal2@illinois.edu>
2021-04-05 21:05:24 -0500

    Added random functions from RTIOW

--- MP3/utils.h
+++ MP3/utils.h
@@ -45,4 +45,32 @@ inline vec3 random_sphere() {
     return vec3(random_double(-1.9, 1.9), random_double(-1.9, 1.9), random_double(-1, -0.1));
 }
 
+inline vec3 random_in_unit_disk() {
+    while (true) {
+        auto p = vec3(random_double(-1,1), random_double(-1,1), 0);
+        if (p.length_squared() >= 1) continue;
+        return p;
+    }
+}
+
+inline vec3 random_in_unit_sphere() {
+    while (true) {
+        auto p = random_vec3(-1,1);
+        if (p.length_squared() >= 1) continue;
+        return p;
+    }
+}
+
+inline vec3 random_unit_vector() {
+    return unit_vector(random_in_unit_sphere());
+}
+
+inline vec3 random_in_hemisphere(const vec3& normal) {
+    vec3 in_unit_sphere = random_in_unit_sphere();
+    if (dot(in_unit_sphere, normal) > 0.0) // In the same hemisphere as the normal
+        return in_unit_sphere;
+    else
+        return -in_unit_sphere;
+}
+
 #endif
--- MP3/vec3.h
+++ MP3/vec3.h
@@ -124,6 +124,13 @@ inline vec3 reflect(vec3 V, vec3 N) {
     return V - 2 * dot(V, N) * N;
 }
 
+inline vec3 refract(const vec3& uv, const vec3& n, double etai_over_etat) {
+    auto cos_theta = fmin(dot(-uv, n), 1.0);
+    vec3 r_out_perp =  etai_over_etat * (uv + cos_theta*n);
+    vec3 r_out_parallel = -sqrt(fabs(1.0 - r_out_perp.length_squared())) * n;
+    return r_out_perp + r_out_parallel;
+}
+
 inline double clip(double n, double lower, double upper) {
     return std::max(lower, std::min(n, upper));
 }
